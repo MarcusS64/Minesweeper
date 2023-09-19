f85f47dd58c44793137d9383309e3e71b184e068
MarcusS64 <marcus@skolds.com>
2023-09-19 11:08:05 +0200

    Add project files.

---
+++ Minesweeper/Minefield.cs
@@ -0,0 +1,119 @@
+﻿using System.Reflection.Metadata;
+
+namespace Minesweeper;
+
+class Minefield
+{
+    private bool[,] _bombLocations = new bool[5, 5];
+    public string[,] mineFieldState = new string[5, 5];
+    private string[,] solutionState = new string[5, 5]; 
+    private static (int x, int y)[] coords = new (int, int)[] { (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1) };
+
+    public void SetBomb(int x, int y)
+    {
+        _bombLocations[x, y] = true;
+    }
+
+    public void SetMineFieldDisplay()
+    {
+        for (int i = 0; i < mineFieldState.GetLength(0); i++) 
+        {
+            for(int j = 0; j < mineFieldState.GetLength(1); j++)
+            {
+                mineFieldState[i, j] = "?";
+
+                if (_bombLocations[i, j] == true)
+                {
+                    solutionState[i, j] = "X";
+                }
+                else
+                {
+                    int numberOfNeighbourBombs = 0;
+                    foreach (var item in coords)
+                    {
+                        int _x = i - item.x;
+                        int _y = j - item.y;
+                        if (_x < mineFieldState.GetLength(0) && _y < mineFieldState.GetLength(1) && _x >= 0 && _y >= 0) //If not then there is no square to connect
+                        {
+                            if (_bombLocations[_x, _y] == true)
+                            {
+                                numberOfNeighbourBombs++;
+                            }
+                        }
+                    }
+
+                    if(numberOfNeighbourBombs > 0)
+                    {
+                        solutionState[i, j] = numberOfNeighbourBombs.ToString();
+                    }
+                    else
+                    {
+                        solutionState[i, j] = " ";
+                    }
+                }
+            }
+        }
+    }
+
+
+    public bool FoundBomb(int x, int y)
+    {
+        if (_bombLocations[x, y] == true)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void UpdateMineFieldDisplay(int x, int y) //Use BFS search to traverse the field
+    {
+
+        mineFieldState[x, y] = solutionState[x, y];
+        if (solutionState[x, y] == " ")
+        {
+            foreach (var item in coords)
+            {
+                int _x = x - item.x;
+                int _y = y - item.y;
+                if (_x < mineFieldState.GetLength(0) && _y < mineFieldState.GetLength(1) && _x >= 0 && _y >= 0)
+                {
+                    if (mineFieldState[_x, _y] == "?")
+                    {
+                        UpdateMineFieldDisplay(_x, _y);
+                    }
+                    
+                }
+            }
+        }                
+    }
+
+    public bool CheckIfAllBombsUncovered()
+    {
+        for (int i = 0; i < mineFieldState.GetLength(0); i++)
+        {
+            for (int j = 0; j < mineFieldState.GetLength(1); j++)
+            {
+                if(mineFieldState[i, j] == "?" && _bombLocations[i, j] == false)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public int GetRowLength()
+    {
+        return mineFieldState.GetLength(0);
+    }
+
+    public bool IsTileUncovered(int x, int y)
+    {
+        if (mineFieldState[x, y] == "?")
+        {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ Minesweeper/Minesweeper.cs
@@ -0,0 +1,114 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.Metrics;
+
+namespace Minesweeper;
+
+class Minesweeper
+{
+    private static string playerGuess;
+    private static int coordinate;
+    private static int[] position = new int[2];
+    static void Main()
+    {
+        var field = new Minefield();
+
+        //set the bombs...
+        field.SetBomb(0, 0);
+        field.SetBomb(0, 1);
+        field.SetBomb(1, 1);
+        field.SetBomb(1, 4);
+        field.SetBomb(4, 2);
+
+        //the mine field should look like this now:
+        //  01234
+        //4|1X1
+        //3|11111
+        //2|2211X
+        //1|XX111
+        //0|X31
+
+        // Game code...
+        field.SetMineFieldDisplay();
+        bool gameOver = false;
+        while (!gameOver)
+        {
+            
+            DrawMineFieldState(field);
+
+            do
+            {
+                Console.WriteLine("Which position do you with to uncover?");
+                playerGuess = Console.ReadLine();
+
+            } while (!CheckInput(field));
+
+            if (field.FoundBomb(position[0], position[1]))
+            {
+                field.UpdateMineFieldDisplay(position[0], position[1]);
+                DrawMineFieldState(field);
+                Console.WriteLine("You found a bomb! Game over");
+                gameOver = true;
+            }
+            else if(field.CheckIfAllBombsUncovered())
+            {                
+                Console.WriteLine("All bombs uncovered... Victory!");
+                gameOver = true;
+            }
+            else
+            {                
+                field.UpdateMineFieldDisplay(position[0], position[1]);
+            }
+
+        }       
+    }
+
+    public static void DrawMineFieldState(Minefield field)
+    {
+        int rowIndex = 4;
+        Console.WriteLine("The new game state is: ");
+        Console.WriteLine("  01234");
+        while (rowIndex > -1)
+        {
+            Console.Write(rowIndex + "|");
+            for (int i = 0; i < field.GetRowLength(); i++)
+            {
+                Console.Write(field.mineFieldState[i, rowIndex]);
+            }
+            rowIndex--;
+            Console.Write("\n");
+
+        }
+    }
+
+    public static bool CheckInput(Minefield field)
+    {
+        if (playerGuess.Length != 2)
+        {
+            Console.WriteLine("Not a valid position input. Please input two numbers only! (Example: 40)");
+            return false;            
+        }
+        else
+        {
+            int index = 0;
+            foreach (char item in playerGuess)
+            {
+                //bool isNumberInput = int.TryParse(item.ToString(), out coordinate);
+                if (!int.TryParse(item.ToString(), out coordinate) || coordinate > 4)
+                {
+                    Console.WriteLine("Not a valid position input. Please input coordinates between 0-4!");
+                    return false;
+                }
+                position[index++] = coordinate;
+            }
+
+            if (field.IsTileUncovered(position[0], position[1]))
+            {
+                Console.WriteLine("Not a valid position input. Tile is already uncovered!");
+                return false;
+            }
+        }
+        
+        
+        return true;
+    }
+}
